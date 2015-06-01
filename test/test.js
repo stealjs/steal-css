3a7e45d5a7b840ff2a86e8ce33efde42cb7f4099
Manuel Mujica <evilsaurer@gmail.com>
2015-06-01 14:57:58 -0300

    Add test and some config files.

---
+++ test/test.js
@@ -0,0 +1,17 @@
+QUnit.module("system-css plugin");
+
+var makeIframe = function(src){
+	var iframe = document.createElement('iframe');
+	window.removeMyself = function(){
+		delete window.removeMyself;
+		document.body.removeChild(iframe);
+	};
+	document.body.appendChild(iframe);
+	iframe.src = src;
+};
+
+asyncTest("url paths in css work", function(){
+	makeIframe("css_paths/site.html");
+});
+
+QUnit.start();
