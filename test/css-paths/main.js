a38ca3499ed3b880990c2e1000662d4e45c27a03
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2017-04-12 08:55:17 -0600

    Dynamic CSS not applied sync in some browsers

--- test/css-paths/main.js
+++ test/css-paths/main.js
@@ -2,80 +2,97 @@ import "./folder/main.css";
 import helpers from 'helpers';
 import steal from "@steal";
 
-var testImage = function(selector, cb){
-	var image = new Image();
-	image.onload = function(){
-		cb();
-	};
-	image.onerror = function(){
-		cb(selector);
-		assert.ok(false, "image not loaded");
-		assert.start();
-		removeMyself();
-	};
-	image.src = $(selector).css("background-image").replace(/url\("?/,"").replace(/"?\)/,"");
+var testImage = function(selector) {
+	return new Promise(function(resolve, reject) {
+		var image = new Image();
+
+		image.onload = function(){
+			resolve();
+		};
+
+		image.onerror = function() {
+			reject(new Error("Cannot load " + selector));
+		};
+
+		image.src = $(selector).css("background-image")
+			.replace(/url\("?/,"")
+			.replace(/"?\)/,"");
+	});
 };
 
+function waitForCssToBeApplied() {
+	return new Promise(function(resolve, reject) {
+		setTimeout(resolve, 300);
+	});
+}
+
+function log() {
+	var btn = $(".btn.btn-danger");
+	console.log("Button display: ", btn.css("display"));
+	console.log("Button backgroundColor: ", btn.css("backgroundColor"));
+
+	console.log("background-image", $("#test-element").css("background-image"));
+	console.log("tilde", $("#test-relative").css("background-image"));
+}
 
 if(steal.isEnv('production')) {
 	if (typeof window !== "undefined" && window.assert) {
 
-		var btn = $('.btn.btn-danger');
-		assert.equal(btn.css('display'), 'inline-block', '@import "locate://"; styles applied');
-		assert.equal(btn.css('backgroundColor'), 'rgb(255, 0, 0)', '@import url("locate://"); styles applied');
-
-		testImage("#test-element", function(err){
-			if(err){
+		waitForCssToBeApplied()
+			.then(function() {
+				var btn = $(".btn.btn-danger");
+				assert.equal(btn.css("display"), "inline-block", '@import "locate://"; styles applied');
+				assert.equal(btn.css("backgroundColor"), "rgb(255, 0, 0)", '@import url("locate://"); styles applied');
+			})
+			.then(function() {
+				return testImage("#test-element");
+			})
+			.then(function() {
+				assert.ok(true, "background-image: url(../); styles applied");
+				return testImage("#test-relative");
+			})
+			.then(function() {
+				assert.ok(true, "background-image: url(locate://); styles applied");
+				done();
+			})
+			.catch(function(err) {
 				assert.ok(false, err);
 				done();
-			} else {
-				assert.ok(true, "background-image: url(../); styles applied");
-				testImage("#test-relative", function(err){
-					if(err){
-						assert.ok(false, err);
-					} else {
-						assert.ok(true, "background-image: url(locate://); styles applied");
-					}
-					done();
-				});
-			}
-		});
+			});
 	} else {
-		console.log("background-image", $("#test-element").css("background-image"));
-		console.log("tilde", $("#test-relative").css("background-image"));
+		waitForCssToBeApplied().then(log);
 	}
 
 	// develop
 } else {
 	// Wait for the @import's to resolve in the <style>
 	// tag added by the main.css! import
-	helpers.waitForCssRules($('style')[0], function () {
-
+	helpers.waitForCssRules($('style')[0], function() {
 		if (typeof window !== "undefined" && window.assert) {
 
-			var btn = $('.btn.btn-danger');
-			assert.equal(btn.css('display'), 'inline-block', '@import "locate://"; styles applied');
-			assert.equal(btn.css('backgroundColor'), 'rgb(255, 0, 0)', '@import url("locate://"); styles applied');
-
-			testImage("#test-element", function(err){
-				if(err){
-					assert.ok(false, err);
-					done();
-				} else {
+			waitForCssToBeApplied()
+				.then(function() {
+					var btn = $('.btn.btn-danger');
+					assert.equal(btn.css("display"), "inline-block", '@import "locate://"; styles applied');
+					assert.equal(btn.css("backgroundColor"), "rgb(255, 0, 0)", '@import url("locate://"); styles applied');
+				})
+				.then(function() {
+					return testImage("#test-element");
+				})
+				.then(function() {
 					assert.ok(true, "background-image: url(../); styles applied");
-					testImage("#test-relative", function(err){
-						if(err){
-							assert.ok(false, err);
-						} else {
-							assert.ok(true, "background-image: url(locate://); styles applied");
-						}
-						done();
-					});
-				}
-			});
+					return testImage("#test-relative");
+				})
+				.then(function() {
+					assert.ok(true, "background-image: url(locate://); styles applied");
+					done();
+				})
+				.catch(function(err) {
+					assert.notOk(err, "should not fail");
+					done();
+				});
 		} else {
-			console.log("background-image", $("#test-element").css("background-image"));
-			console.log("tilde", $("#test-relative").css("background-image"));
+			waitForCssToBeApplied().then(log);
 		}
 	});
 }
